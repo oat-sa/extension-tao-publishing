8786ff8efd407a7332687ee2ea4ea79ebb1bc09a
Gyula Szucs <gyula@taotesting.com>
2018-06-08 15:34:07 +0200

    TAO-5790 Bump version

--- manifest.php
+++ manifest.php
@@ -24,12 +24,11 @@ return array(
 	'label' => 'Test Publishing',
 	'description' => 'An extension to publish tests to a delivery environment',
     'license' => 'GPL-2.0',
-    'version' => '0.8.1',
+    'version' => '0.9.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'taoDeliveryRdf' => '>=3.20.0',
-        'taoTaskQueue' => '>=0.13.1',
-        'tao' => '>=17.1.0'
+        'tao' => '>=19.5.0'
     ),
 	'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoPublishingManager',
     'acl' => array(
