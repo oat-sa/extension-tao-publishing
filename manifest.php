c58e94dbd8263d436d45b7094e750ed914580fda
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2018-10-08 12:08:33 +0200

    version bump

--- manifest.php
+++ manifest.php
@@ -1,34 +1,34 @@
 <?php
-/**  
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
  * of the License (non-upgradable).
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- * 
+ *
  * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
- *               
- * 
- */               
+ *
+ *
+ */
 
 return array(
     'name' => 'taoPublishing',
 	'label' => 'Test Publishing',
 	'description' => 'An extension to publish tests to a delivery environment',
     'license' => 'GPL-2.0',
-    'version' => '0.9.2',
+    'version' => '1.0.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
-        'taoDeliveryRdf' => '>=3.20.0',
-        'tao' => '>=19.8.0'
+        'taoDeliveryRdf' => '>=6.0.0',
+        'tao' => '>=21.0.0'
     ),
 	'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoPublishingManager',
     'acl' => array(
@@ -54,10 +54,10 @@ return array(
 	'constants' => array(
 	    # views directory
 	    "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
-	    
+
 		#BASE URL (usually the domain root)
 		'BASE_URL' => ROOT_URL.'taoPublishing/',
-	    
+
 	    #BASE WWW required by JS
 	    'BASE_WWW' => ROOT_URL.'taoPublishing/views/'
 	),
