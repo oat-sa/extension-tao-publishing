88e59770468fefa4ae4e5b63ca2b92ff160d00fd
Aleksej Tikhanovich <aleksej@taotesting.com>
2018-11-22 16:04:51 +0300

    Extend generis search

---
+++ model/search/GenerisSearch.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2018 (original work) Open Assessment Technologies SA;
+ *
+ *
+ */
+namespace oat\taoPublishing\model\search;
+
+use oat\generis\model\OntologyRdfs;
+use oat\tao\model\search\ResultSet;
+use oat\tao\model\search\strategy\GenerisSearch as OriginGenerisSearch;
+use oat\generis\model\OntologyAwareTrait;
+use oat\taoPublishing\model\publishing\delivery\PublishingDeliveryService;
+
+
+class GenerisSearch extends OriginGenerisSearch
+{
+    use OntologyAwareTrait;
+
+    /**
+     * (non-PHPdoc)
+     * @see \oat\tao\model\search\Search::query()
+     */
+    public function query($queryString, $type, $start = 0, $count = 10, $order = 'id', $dir = 'DESC') {
+        /** @var ResultSet $result */
+        $result = parent::query($queryString, $type, $start, $count, $order, $dir);
+        if (!$result->count()) {
+            $rootClass = $this->getClass($type);
+            $results = $rootClass->searchInstances([
+                PublishingDeliveryService::ORIGIN_DELIVERY_ID_FIELD => $queryString,
+                PublishingDeliveryService::ORIGIN_TEST_ID_FIELD => $queryString,
+            ], array(
+                'recursive' => true,
+                'like'      => true,
+                'chaining' => 'or',
+                'offset'    => $start,
+                'limit'     => $count,
+            ));
+            $ids = array();
+            foreach ($results as $resource) {
+                $ids[] = $resource->getUri();
+            }
+
+            return new ResultSet($ids, $this->getTotalCount($queryString, $rootClass));
+        }
+        return $result;
+    }
+
+    /**
+     * Return total count of corresponded instances
+     *
+     * @param string $queryString
+     * @param \core_kernel_classes_Class $rootClass
+     *
+     * @return array
+     */
+    private function getTotalCount($queryString, $rootClass = null )
+    {
+        return $rootClass->countInstances(
+            array(
+                PublishingDeliveryService::ORIGIN_DELIVERY_ID_FIELD => $queryString,
+                PublishingDeliveryService::ORIGIN_TEST_ID_FIELD => $queryString,
+            ),
+            array(
+                'recursive' => true,
+                'like'      => true,
+                'chaining' => 'or'
+            )
+        );
+    }
+}
