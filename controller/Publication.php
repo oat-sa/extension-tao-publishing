194a88545fa9283300231e939a8f80f5c94e1e92
Andre Machado <andre.machado@taotesting.com>
2020-04-06 13:51:28 +0200

    create Publication controller

---
+++ controller/Publication.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace oat\taoPublishing\controller;
+
+class Publication extends \tao_actions_SaSModule
+{
+
+    public function publishDelivery(): void {
+        die('hello world!!!');
+    }
+
+}
