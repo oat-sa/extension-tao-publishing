72839260e4f54449bd3bfb2db688d53d07550a0f
Andrew Shapiro <shpr.andrew@gmail.com>
2020-09-14 18:44:54 +0300

    Fix/BSA-227/Bump versions

--- manifest.php
+++ manifest.php
@@ -32,10 +32,10 @@ return array(
 	'label' => 'Test Publishing',
 	'description' => 'An extension to publish tests to a delivery environment',
     'license' => 'GPL-2.0',
-    'version' => '5.0.0',
+    'version' => '5.1.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => array(
-        'taoDeliveryRdf' => '>=12.2.1',
+        'taoDeliveryRdf' => '>=13.1.0',
         'tao' => '>=45.0.0',
         'taoQtiTest' => '>=38.13.0',
     ),
