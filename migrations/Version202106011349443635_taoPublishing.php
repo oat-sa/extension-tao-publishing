6e351ae073eb56e0b26f6aa559f9d18d74c59b76
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2021-06-01 17:10:30 +0200

    feat: register the taoPublishing search using proxy

---
+++ migrations/Version202106011349443635_taoPublishing.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoPublishing\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use oat\tao\model\search\Search;
+use oat\tao\model\search\SearchProxy;
+use oat\tao\model\search\strategy\GenerisSearch as OriginalGenerisSearch;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+use oat\taoPublishing\model\search\GenerisSearch;
+
+final class Version202106011349443635_taoPublishing extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Registering listener for RemoteDeliveryCreatedEvent';
+    }
+
+    public function up(Schema $schema): void
+    {
+        /** @var SearchProxy $searchProxy */
+        $searchProxy = $this->getServiceManager()->get(SearchProxy::SERVICE_ID);
+
+        /** @var OriginalGenerisSearch $generisSearch */
+        $generisSearch = $searchProxy->getOption(SearchProxy::OPTION_DEFAULT_SEARCH_CLASS);
+
+        if ($generisSearch instanceof OriginalGenerisSearch) {
+            $searchProxy->setOption(SearchProxy::OPTION_DEFAULT_SEARCH_CLASS, new GenerisSearch($generisSearch->getOptions()));
+
+            $this->getServiceManager()->register(Search::SERVICE_ID, $searchProxy);
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+        /** @var SearchProxy $searchProxy */
+        $searchProxy = $this->getServiceManager()->get(SearchProxy::SERVICE_ID);
+        $searchProxy->setOption(SearchProxy::OPTION_DEFAULT_SEARCH_CLASS, OriginalGenerisSearch::class);
+
+        $this->getServiceManager()->register(SearchProxy::SERVICE_ID, $searchProxy);
+    }
+}
--- scripts/install/RegisterGenerisSearch.php
+++ scripts/install/RegisterGenerisSearch.php
@@ -4,7 +4,9 @@ namespace oat\taoPublishing\scripts\install;
 
 use oat\oatbox\extension\InstallAction;
 use oat\tao\model\search\Search;
-use oat\tao\model\search\strategy\GenerisSearch;
+use oat\tao\model\search\SearchProxy;
+use oat\tao\model\search\strategy\GenerisSearch as OriginalGenerisSearch;
+use oat\taoPublishing\model\search\GenerisSearch;
 
 /**
  * Class RegisterGenerisSearch
@@ -14,10 +16,16 @@ class RegisterGenerisSearch extends InstallAction
 {
     public function __invoke($params)
     {
-        $searchService = $this->getServiceLocator()->get(Search::SERVICE_ID);
-        if ($searchService instanceof GenerisSearch) {
-            $newSearchService = new \oat\taoPublishing\model\search\GenerisSearch($searchService->getOptions());
-            $this->getServiceManager()->register(Search::SERVICE_ID, $newSearchService);
+        /** @var SearchProxy $searchProxy */
+        $searchProxy = $this->getServiceManager()->get(SearchProxy::SERVICE_ID);
+
+        /** @var OriginalGenerisSearch $generisSearch */
+        $generisSearch = $searchProxy->getOption(SearchProxy::OPTION_DEFAULT_SEARCH_CLASS);
+
+        if ($generisSearch instanceof OriginalGenerisSearch) {
+            $searchProxy->setOption(SearchProxy::OPTION_DEFAULT_SEARCH_CLASS, new GenerisSearch($generisSearch->getOptions()));
+
+            $this->getServiceManager()->register(Search::SERVICE_ID, $searchProxy);
         }
 
         return new \common_report_Report(\common_report_Report::TYPE_SUCCESS, 'Generis search service registered');
